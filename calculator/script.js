ef0680da4fd6c3d540b21e18175ea21eb065c8ea
suraj <sysnicpro123@gmail.com>
2023-07-14 05:54:50 +0530

    calculator app

---
+++ calculator/script.js
@@ -0,0 +1,26 @@
+const buttonsEl = document.querySelectorAll("button");
+const inputFieldEl = document.getElementById("result");
+
+for (let i = 0; i < buttonsEl.length; i++) {
+  buttonsEl[i].addEventListener("click", () => {
+    const buttonValue = buttonsEl[i].textContent;
+
+    if (buttonValue == "C") {
+      clearResults();
+    } else if (buttonValue == "=") {
+      calculateResults();
+    } else {
+      appendValue(buttonValue);
+    }
+  });
+}
+
+const clearResults = () => {
+  inputFieldEl.value = "";
+};
+const calculateResults = () => {
+  inputFieldEl.value = eval(inputFieldEl.value);
+};
+const appendValue = (buttonValue) => {
+  inputFieldEl.value += buttonValue;
+};
